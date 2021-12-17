a1e4f25395b749345f6949dd5cc2dff719835536
Kimberly Arcena <kimanecra143@gmail.com>
2021-12-17 17:27:32 +0800

    Animation Splash Screen

--- app/src/main/java/com/example/bigproject/MainActivity.java
+++ app/src/main/java/com/example/bigproject/MainActivity.java
@@ -2,13 +2,50 @@ package com.example.bigproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
+    private  static  int SPLASH_SCREEN = 2000;
+
+    //variables
+    Animation topAnim, bottomAnim;
+    ImageView imageLogo;
+    TextView logo, slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //remove the status and action bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        imageLogo = findViewById(R.id.imageView);
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.slogan);
+
+        imageLogo.setAnimation(topAnim);
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
+
 }
