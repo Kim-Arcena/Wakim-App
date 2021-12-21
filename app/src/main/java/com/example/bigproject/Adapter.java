87d8a256f4b50c65b459741a005b37ffbee84e4e
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-21 22:33:45 +0800

    Adapter and Model

---
+++ app/src/main/java/com/example/bigproject/Adapter.java
@@ -0,0 +1,62 @@
+package com.example.bigproject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+    Context context;
+    Activity activity;
+    List<Model> notesList;          //take list of model types
+
+    public Adapter(Context context, Activity activity, List<Model> notesList) {
+        this.context = context;
+        this.activity = activity;
+        this.notesList = notesList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull  ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_view_layout, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.MyViewHolder holder, int position) {
+        //set the title and description from the list using getter and setter methods
+        holder.title.setText(notesList.get(position).getTitle());
+        holder.description.setText(notesList.get(position).getDescription());
+
+    }
+    //returns the item count of the list
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView title, description;
+        RelativeLayout layout;
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+
+            title = itemView.findViewById(R.id.text_title);
+            description = itemView.findViewById(R.id.text_description);
+            layout = itemView.findViewById(R.id.relative_layout);
+
+        }
+    }
+}
--- app/src/main/java/com/example/bigproject/Dashboard.java
+++ app/src/main/java/com/example/bigproject/Dashboard.java
@@ -2,20 +2,53 @@ package com.example.bigproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Dashboard extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FloatingActionButton fab;
+    Adapter adapter;
+    List<Model> noteList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        recyclerView = findViewById(R.id.recycle_view);
+        fab = findViewById(R.id.floating_action_bar);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Dashboard.this, AddAlarmActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        noteList = new ArrayList<>();
+        //set manager layout to recycleView
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this, Dashboard.this, noteList);
+        recyclerView.setAdapter(adapter);
+
     }
 
+    //Menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.options_menu, menu);
