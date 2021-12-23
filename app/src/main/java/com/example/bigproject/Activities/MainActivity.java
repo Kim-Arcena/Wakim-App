87a7f2350668b461d572653045a1e50b1dbed070
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-23 22:30:21 +0800

    Separate Splash Screen

--- app/src/main/java/com/example/bigproject/Activities/MainActivity.java
+++ app/src/main/java/com/example/bigproject/Activities/MainActivity.java
@@ -1,52 +1,17 @@
 package com.example.bigproject.Activities;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.WindowManager;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
-import android.widget.TextView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bigproject.R;
 
 public class MainActivity extends AppCompatActivity {
-    private  static  int SPLASH_SCREEN = 2000;
-
-    //variables
-    Animation topAnim, bottomAnim;
-    ImageView imageLogo;
-    TextView logo, slogan;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //remove the status and action bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
-        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
-        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
-
-        imageLogo = findViewById(R.id.imageView);
-        logo = findViewById(R.id.logo);
-        slogan = findViewById(R.id.slogan);
-
-        imageLogo.setAnimation(topAnim);
-        logo.setAnimation(topAnim);
-        slogan.setAnimation(bottomAnim);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, Dashboard.class);
-                startActivity(intent);
-                finish();
-            }
-        }, SPLASH_SCREEN);
     }
-
 }
