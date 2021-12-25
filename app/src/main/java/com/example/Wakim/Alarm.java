5bb898edd7ef3366a702fd785f8abd5de8abb4f4
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-25 18:54:10 +0800

    Alarm Class

---
+++ app/src/main/java/com/example/Wakim/Alarm.java
@@ -0,0 +1,102 @@
+package com.example.Wakim;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.Calendar;
+
+@Entity(tableName = "alarm_table")
+public class Alarm {
+    @PrimaryKey
+    @NonNull
+    private int alarmId;
+
+    private int hour, minute;
+    private boolean started, recurring;
+    private boolean monday, tuesday, wednesday, thursday, friday, saturday, sunday;
+    private String title;
+
+    private long created;
+
+    public Alarm(int alarmId, int hour, int minute, String title, long created, boolean started, boolean recurring, boolean monday, boolean tuesday, boolean wednesday, boolean thursday, boolean friday, boolean saturday, boolean sunday) {
+        this.alarmId = alarmId;
+        this.hour = hour;
+        this.minute = minute;
+        this.started = started;
+
+        this.recurring = recurring;
+
+        this.monday = monday;
+        this.tuesday = tuesday;
+        this.wednesday = wednesday;
+        this.thursday = thursday;
+        this.friday = friday;
+        this.saturday = saturday;
+        this.sunday = sunday;
+
+        this.title = title;
+
+        this.created = created;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public boolean isStarted() {
+        return started;
+    }
+
+    public int getAlarmId() {
+        return alarmId;
+    }
+
+    public void setAlarmId(int alarmId) {
+        this.alarmId = alarmId;
+    }
+
+    public boolean isRecurring() {
+        return recurring;
+    }
+
+    public boolean isMonday() {
+        return monday;
+    }
+
+    public boolean isTuesday() {
+        return tuesday;
+    }
+
+    public boolean isWednesday() {
+        return wednesday;
+    }
+
+    public boolean isThursday() {
+        return thursday;
+    }
+
+    public boolean isFriday() {
+        return friday;
+    }
+
+    public boolean isSaturday() {
+        return saturday;
+    }
+
+    public boolean isSunday() {
+        return sunday;
+    }
+
+}
--- app/src/main/java/com/example/Wakim/createAlarm/CreateAlarmFragment.java
+++ app/src/main/java/com/example/Wakim/createAlarm/CreateAlarmFragment.java
@@ -2,67 +2,94 @@ package com.example.Wakim.createAlarm;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TimePicker;
 
+import com.example.Wakim.Alarm;
 import com.example.Wakim.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CreateAlarmFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.Random;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class CreateAlarmFragment extends Fragment {
-
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public CreateAlarmFragment() {
-        // Required empty public constructor
-    }
+    @BindView(R.id.fragment_createalarm_timePicker) TimePicker timePicker;
+    @BindView(R.id.fragment_createalarm_title) EditText title;
+    @BindView(R.id.fragment_createalarm_scheduleAlarm) Button scheduleAlarm;
+    @BindView(R.id.fragment_createalarm_recurring) CheckBox recurring;
+    @BindView(R.id.fragment_createalarm_checkMon) CheckBox mon;
+    @BindView(R.id.fragment_createalarm_checkTue) CheckBox tue;
+    @BindView(R.id.fragment_createalarm_checkWed) CheckBox wed;
+    @BindView(R.id.fragment_createalarm_checkThu) CheckBox thu;
+    @BindView(R.id.fragment_createalarm_checkFri) CheckBox fri;
+    @BindView(R.id.fragment_createalarm_checkSat) CheckBox sat;
+    @BindView(R.id.fragment_createalarm_checkSun) CheckBox sun;
+    @BindView(R.id.fragment_createalarm_recurring_options) LinearLayout recurringOptions;
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
+     * Called to have the fragment instantiate its user interface view.
+     * This is optional, and non-graphical fragments can return null. This will be called between
+     * {@link #onCreate(Bundle)} and {@link #onViewCreated(View, Bundle)}.
+     * <p>A default View can be returned by calling {@link #Fragment(int)} in your
+     * constructor. Otherwise, this method returns null.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment createAlarm.
+     * <p>It is recommended to <strong>only</strong> inflate the layout in this method and move
+     * logic that operates on the returned View to {@link #onViewCreated(View, Bundle)}.
+     *
+     * <p>If you return a View from here, you will later be called in
+     * {@link #onDestroyView} when the view is being released.
+     *
+     * @param inflater           The LayoutInflater object that can be used to inflate
+     *                           any views in the fragment,
+     * @param container          If non-null, this is the parent view that the fragment's
+     *                           UI should be attached to.  The fragment should not add the view itself,
+     *                           but this can be used to generate the LayoutParams of the view.
+     * @param savedInstanceState If non-null, this fragment is being re-constructed
+     *                           from a previous saved state as given here.
+     * @return Return the View for the fragment's UI, or null.
      */
-    // TODO: Rename and change types and number of parameters
-    public static CreateAlarmFragment newInstance(String param1, String param2) {
-        CreateAlarmFragment fragment = new CreateAlarmFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_create_alarm, container, false);
+
+        ButterKnife.bind(this, view);
+
+        recurring.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(isChecked){
+                    recurringOptions.setVisibility(View.VISIBLE);
+                }
+                else{
+                    recurringOptions.setVisibility(View.GONE);
+                }
+            }
+        });
+        scheduleAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scheduleAlarm();
+            }
+        });
+        return view;
+
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private void scheduleAlarm() {
+        int alarmId = new Random().nextInt(Integer.MAX_VALUE);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_alarm, container, false);
     }
 }
