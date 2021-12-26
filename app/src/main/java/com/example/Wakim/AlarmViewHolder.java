bb31bf50786380ae345f25f7dd41c2041d186ac7
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-26 16:51:06 +0800

    Created item_alarm.xml

--- app/src/main/java/com/example/Wakim/AlarmViewHolder.java
+++ app/src/main/java/com/example/Wakim/AlarmViewHolder.java
@@ -1,6 +1,14 @@
 package com.example.Wakim;
 
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.jetbrains.annotations.NotNull;
+
 public class AlarmViewHolder extends RecyclerView.ViewHolder {
+    public AlarmViewHolder(@NonNull @NotNull View itemView) {
+        super(itemView);
+    }
 }
