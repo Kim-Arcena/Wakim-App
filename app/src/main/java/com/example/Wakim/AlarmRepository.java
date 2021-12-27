778d38471203060cfb3fa1e17309269ccdc7d39c
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-27 19:26:26 +0800

    Retrieve records from DB

--- app/src/main/java/com/example/Wakim/AlarmRepository.java
+++ app/src/main/java/com/example/Wakim/AlarmRepository.java
@@ -1,6 +1,36 @@
 package com.example.Wakim;
 
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.example.Wakim.createAlarm.AlarmDatabase;
+
+import java.util.List;
+
 public class AlarmRepository {
-    public AlarmRepository(Object p0) {
+    private AlarmDao alarmDao;
+    private LiveData<List<Alarm>> alarmsLiveData;
+
+    public AlarmRepository(Application application) {
+        AlarmDatabase db = AlarmDatabase.getDatabase(application);
+        alarmDao = db.alarmDao();
+        alarmsLiveData = alarmDao.getAlarms();
+    }
+
+    public void insert(Alarm alarm) {
+        AlarmDatabase.databaseWriteExecutor.execute(() -> {
+            alarmDao.insert(alarm);
+        });
+    }
+
+    public void update(Alarm alarm) {
+        AlarmDatabase.databaseWriteExecutor.execute(() -> {
+            alarmDao.update(alarm);
+        });
+    }
+
+    public LiveData<List<Alarm>> getAlarmsLiveData() {
+        return alarmsLiveData;
     }
 }
--- app/src/main/java/com/example/Wakim/createAlarm/AlarmDatabase.java
+++ app/src/main/java/com/example/Wakim/createAlarm/AlarmDatabase.java
@@ -2,27 +2,30 @@ package com.example.Wakim.createAlarm;
 
 import android.content.Context;
 
+import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.Wakim.Alarm;
 import com.example.Wakim.AlarmDao;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Database(entities = {Alarm.class}, version = 1, exportSchema = false)
 public abstract class AlarmDatabase extends RoomDatabase {
     public abstract AlarmDao alarmDao();
 
     private static volatile AlarmDatabase INSTANCE;
     private static final int NUMBER_OF_THREADS = 4;
-    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
     /**
      *
      * @param context
      * @return
      */
-    static AlarmDatabase getDatabase(final Context context) {
+    public static AlarmDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (AlarmDatabase.class) {
                 if (INSTANCE == null) {
