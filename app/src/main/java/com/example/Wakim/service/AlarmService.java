7ac2e3a04972f70b3122b8da0ee0ef171e6c7b7d
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-29 02:23:44 +0800

    Add MediaPlayer and Vibrator

--- app/src/main/java/com/example/Wakim/service/AlarmService.java
+++ app/src/main/java/com/example/Wakim/service/AlarmService.java
@@ -1,4 +1,39 @@
 package com.example.Wakim.service;
 
-public class AlarmService {
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.os.Vibrator;
+
+import androidx.annotation.Nullable;
+
+import com.example.Wakim.R;
+
+public class AlarmService extends Service {
+    private MediaPlayer mediaPlayer;
+    private Vibrator vibrator;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
+        mediaPlayer.setLooping(true);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+        vibrator.cancel();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
