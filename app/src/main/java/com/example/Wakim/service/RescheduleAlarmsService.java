672ce86bd902eae8f6810853daf3ebc33b70609b
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-29 02:58:26 +0800

    Reschedule Alarms Service

--- app/src/main/java/com/example/Wakim/service/RescheduleAlarmsService.java
+++ app/src/main/java/com/example/Wakim/service/RescheduleAlarmsService.java
@@ -6,8 +6,14 @@ import android.os.IBinder;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.LifecycleService;
+import androidx.lifecycle.Observer;
+
+import com.example.Wakim.data.Alarm;
+import com.example.Wakim.data.AlarmRepository;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class RescheduleAlarmsService extends LifecycleService {
 
@@ -25,7 +31,22 @@ public class RescheduleAlarmsService extends LifecycleService {
 
     @Override
     public int onStartCommand(@Nullable @org.jetbrains.annotations.Nullable Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        super.onStartCommand(intent, flags, startId);
+
+        AlarmRepository alarmRepository = new AlarmRepository(getApplication());
+
+        alarmRepository.getAlarmsLiveData().observe(this, new Observer<List<Alarm>>() {
+            @Override
+            public void onChanged(List<Alarm> alarms) {
+                for (Alarm a : alarms) {
+                    if (a.isStarted()) {
+                        a.schedule(getApplicationContext());
+                    }
+                }
+            }
+        });
+
+        return START_STICKY;
     }
     @Override
     public void onDestroy() {
