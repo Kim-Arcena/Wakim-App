e41478de9803f1556882686883a0d7a962b3bf48
Kim-Arcena <kvarcena@up.edu.ph>
2021-12-29 02:27:56 +0800

    Create RingActivity

---
+++ app/src/main/java/com/example/Wakim/Activities/RingActivity.java
@@ -0,0 +1,16 @@
+package com.example.Wakim.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.Wakim.R;
+
+public class RingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ring);
+    }
+}
