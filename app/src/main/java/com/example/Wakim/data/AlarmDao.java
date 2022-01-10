8de287cf5ce891406528df5d4721f17be7f062fd
Kim-Arcena <kvarcena@up.edu.ph>
2022-01-10 20:51:30 +0800

    Sort by hour and minute

--- app/src/main/java/com/example/Wakim/data/AlarmDao.java
+++ app/src/main/java/com/example/Wakim/data/AlarmDao.java
@@ -21,7 +21,7 @@ public interface AlarmDao {
     @Query("DELETE FROM alarm_table")
     void deleteAll();
 
-    @Query("SELECT * FROM alarm_table ORDER BY created ASC")
+    @Query("SELECT * FROM alarm_table ORDER BY hour, minute ASC")
     LiveData<List<Alarm>> getAlarms();
 
     @Update
